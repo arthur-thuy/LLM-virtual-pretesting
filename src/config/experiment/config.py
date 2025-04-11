91cae805354f613797533a9d915f2c8738fa8909
abthuy <arthur.thuy@ugent.be>
2025-04-11 17:14:41 +0200

    run

--- src/config/experiment/config.py
+++ src/config/experiment/config.py
@@ -47,7 +47,7 @@ _C.EXAMPLE_FORMATTER.NAME = "quotes"
 # example selector
 _C.EXAMPLE_SELECTOR = CN()
 # example selector name
-_C.EXAMPLE_SELECTOR.NAME = "random"
+_C.EXAMPLE_SELECTOR.NAME = "studentid_recency"  # "random"
 # number of examples to select
 _C.EXAMPLE_SELECTOR.NUM_EXAMPLES = 3
 
