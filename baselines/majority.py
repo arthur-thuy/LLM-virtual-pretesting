56b1166405a75116b3cfa39e97ad388eb4ce69f4
Luca Benedetto <luca.benedetto93@gmail.com>
2025-06-26 15:21:37 +0100

    add code for majority baseline (using randomly sampled student histories)

---
+++ baselines/majority.py
@@ -0,0 +1,54 @@
+from typing import List, Tuple
+import pandas as pd
+import numpy as np
+
+
+def majority_prediction_answer_correctness(
+    df: pd.DataFrame, 
+    train_df: pd.DataFrame,
+    history_len: int = 1,
+    correct_prediction_threshold: float = 0.5
+) -> Tuple[List[bool], List[bool]]:
+    
+    list_predicted_correctness = []
+    list_student_option_correct = []
+
+    for student_id, student_option_correct, time in df[['student_id', 'student_option_correct', 'time']].values:
+        # Item selector
+        examples_df = train_df[(train_df['student_id']==student_id)&(train_df['time']<time)]
+        if len(examples_df) < history_len:
+            print(
+                "[Warning] Training dataset for student %s is smaller (%d) than history len (%d)." 
+                % (student_id, len(examples_df), history_len)
+            )
+            continue
+        examples_df = examples_df.sample(n=history_len)
+
+        # Predict answer correctness
+        predicted_correctness = examples_df['student_option_correct'].mean() >= correct_prediction_threshold
+        list_predicted_correctness.append(predicted_correctness)
+        list_student_option_correct.append(student_option_correct)
+
+    if len(list_predicted_correctness) != len(list_student_option_correct):
+        raise ValueError("The two lists predictions and correctness do not have the same length.")
+
+    return list_predicted_correctness, list_student_option_correct
+
+
+if __name__ == '__main__':
+
+    train_df = pd.read_csv('data/gold/dbe_kt22_interactions_train.csv')
+
+    split_to_data_path = {
+        'val_small': 'data/gold/dbe_kt22_interactions_valsmall.csv',
+        'val_large': 'data/gold/dbe_kt22_interactions_vallarge.csv',
+        'test': 'data/gold/dbe_kt22_interactions_test.csv',
+    }
+
+    for data_split in split_to_data_path.keys():
+        print("Data split: %s" % data_split)
+        for history_length in [1, 3, 5]:
+            print("HISTORY LENGTH = %d" % history_length)
+            df = pd.read_csv(split_to_data_path[data_split])
+            predictions, correctness = majority_prediction_answer_correctness(df, train_df, history_len=history_length)
+            print("ACC = %.4f" % (np.mean([predictions[i] == correctness[i] for i in range(len(predictions))])))
