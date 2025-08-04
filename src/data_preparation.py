e2924fa2eec72c722925481d0eda360f10a45fe3
abthuy <arthur.thuy@ugent.be>
2025-08-04 20:02:54 +0200

    try CFE reading

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -13,7 +13,7 @@ from tools.constants import (
     GOLD_DIR,
     SILVER_DIR,
 )
-from tools.data_manager import CupaDatamanager, DBEKT22Datamanager
+from tools.data_manager import CupaCFEDatamanager, DBEKT22Datamanager
 
 # set logger
 logger = structlog.get_logger()
@@ -22,34 +22,35 @@ logger = structlog.get_logger()
 def main():
     """Run the data preparation."""
 
-    # # CUPA
-    # logger.info("Starting preparation CUPA")
-    # cupa_dm = CupaDatamanager()
-    # _, _ = cupa_dm.build_dataset(read_dir=BRONZE_DIR, write_dir=SILVER_DIR)
+    # # DBE-KT22
+    # logger.info("Starting preparation DBE-KT22")
+    # # NOTE: already built
+    # dbekt22_dm = DBEKT22Datamanager()
+    # _, _ = dbekt22_dm.build_dataset(read_dir=BRONZE_DIR, write_dir=SILVER_DIR)
+    # logger.info("Processing DBE-KT22 for student behaviour replication and roleplay")
+    # # create fixed split
+    # data_loader = DataLoader(
+    #     read_dir=SILVER_DIR,
+    #     write_dir=GOLD_DIR,
+    #     dataset_name="dbe_kt22",
+    # )
+    # data_loader.split_data(
+    #     val_size_question=0.15,
+    #     test_size_question=0.25,
+    #     val_size_interact=600,
+    #     valsmall_size_interact=100,
+    #     test_size_interact=1000,
+    #     split_interactions=True,
+    #     stratified=True,
+    #     seed=42,
+    #     join_key="question_id",
+    # )
 
-    # DBE-KT22
-    logger.info("Starting preparation DBE-KT22")
-    # NOTE: already built
-    dbekt22_dm = DBEKT22Datamanager()
-    _, _ = dbekt22_dm.build_dataset(read_dir=BRONZE_DIR, write_dir=SILVER_DIR)
-    logger.info("Processing DBE-KT22 for student behaviour replication and roleplay")
-    # create fixed split
-    data_loader = DataLoader(
-        read_dir=SILVER_DIR,
-        write_dir=GOLD_DIR,
-        dataset_name="dbe_kt22",
-    )
-    data_loader.split_data(
-        val_size_question=0.15,
-        test_size_question=0.25,
-        val_size_interact=600,
-        valsmall_size_interact=100,
-        test_size_interact=1000,
-        split_interactions=True,
-        stratified=True,
-        seed=42,
-        join_key="question_id",
-    )
+    # CUPA-CFE
+    logger.info("Starting preparation CUPA-CFE")
+    cupacfe_dm = CupaCFEDatamanager()
+    _, _ = cupacfe_dm.build_dataset(read_dir=BRONZE_DIR, write_dir=SILVER_DIR)
+    # TODO: split data with split_interactions=False
 
 
 if __name__ == "__main__":
