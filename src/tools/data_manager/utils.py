73e65b4ac3ebfc74dbd26d62587d1b20e9de3f91
abthuy <arthur.thuy@ugent.be>
2025-08-06 16:02:06 +0200

    move to file

--- src/tools/data_manager/utils.py
+++ src/tools/data_manager/utils.py
@@ -30,3 +30,13 @@ def count_answer_options(row, df_question_choice: pd.DataFrame) -> int:
         Number of answer options.
     """
     return len(df_question_choice[df_question_choice["question_id"] == row["id"]])
+
+
+def bring_correct_option_forward(row):
+    """
+    Bring the correct option to the front of the option_texts list.
+    """
+    correct_option_index = row["correct_option_id"] - 1  # Convert to zero-based index
+    row["option_texts"].insert(0, row["option_texts"].pop(correct_option_index))
+    row["correct_option_id"] = 1  # Update correct option index to 1 (first position)
+    return row
